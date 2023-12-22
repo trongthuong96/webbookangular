8292a69a009c1e1ad3cce70c7f8200ad809f6bd6
trongthuong96 <dotrongthuong8@gmail.com>
2023-12-22 14:27:12 +0700

    modified:   src/app/app.routes.ts 	modified:   vercel.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    //{ path: '', pathMatch: 'full', redirectTo: '' },
+    { path: '', pathMatch: 'full', redirectTo: 'trang-chu' },
     {
-        path: '',
+        path: 'trang-chu',
         loadComponent: () => import('./components/home/home.component')
             .then(mod => mod.HomeComponent),
         data: { reuse: true, title: "Truyện Mới"} 
