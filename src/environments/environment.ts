971ecca609a723a093a4c081c2b2bbc3c93050aa
trongthuong96 <dotrongthuong8@gmail.com>
2023-12-29 21:59:16 +0700

    change api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
     production: true,
     //apiUrl: 'https://localhost:7131/api', // Đường dẫn đến máy chủ proxy trên production
-    apiUrl: 'https://api-url.truyenmoi.click/api', // Đường dẫn đến máy chủ proxy trên production
-    //apiUrl: 'http://localhost:4200/api',
+    //apiUrl: 'https://api-url.truyenmoi.click/api', // Đường dẫn đến máy chủ proxy trên production
+    apiUrl: 'https://truyenhay.bsite.net/api',
 };
   
