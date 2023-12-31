0addcb4d8209b455839040f69d9fb4365a3e60ce
trongthuong96 <dotrongthuong8@gmail.com>
2023-12-31 19:58:42 +0700

    modified:   src/app/components/home/home.component.html 	modified:   src/app/config/SignatureInterceptor.ts

--- src/app/config/SignatureInterceptor.ts
+++ src/app/config/SignatureInterceptor.ts
@@ -7,7 +7,6 @@ import { SignatureService } from '../services/signature.service';
 @Injectable()
 export class SignatureInterceptor implements HttpInterceptor {
   
-  private lastSignatureTime: number = 0; // Lưu thời điểm cuối cùng chữ ký được tạo
   private maxRetryAttempts = 2; // Số lần thử lại tối đa
   private retryDelay = 1000; // Thời gian chờ giữa các lần thử lại (ms)
 
@@ -18,26 +17,8 @@ export class SignatureInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
 
-    // Import thư viện Date từ thư viện JavaScript
-    const currentDate = new Date();
-
-    // Lấy số phút
-    const minutes = currentDate.getMinutes() + 1026;
-
-    const dataForSignature = minutes.toString();
-    // Tạo chữ ký sử dụng service hoặc logic bạn đã có
-    const signature = this.signatureService.generateSignature(dataForSignature);
-
-    // Thêm chữ ký vào header
-    const signedRequest = request.clone({
-      setHeaders: {
-        'Content-Type': 'application/json',
-        'X-Signature': signature,
-      },
-    });
-
     // Intercept và thử lại lần 1
-    return next.handle(signedRequest)
+    return next.handle(this.addSignature(request))
       .pipe(
         retryWhen(errors => errors.pipe(
           mergeMap((error, i) => {
@@ -52,8 +33,30 @@ export class SignatureInterceptor implements HttpInterceptor {
         catchError((error) => {
           // Xử lý lỗi sau khi đã thử lại
           // Có thể thêm logic xử lý lỗi ở đây
+          // Nếu muốn thử lại, gọi this.intercept() lại ở đây
+          // return this.intercept(request, next);
           return throwError(error);
         })
       );
+  }
+
+  private addSignature(request: HttpRequest<any>): HttpRequest<any> {
+    // Import thư viện Date từ thư viện JavaScript
+    const currentDate = new Date();
+
+    // Lấy số phút
+    const minutes = currentDate.getMinutes() + 1026;
+
+    const dataForSignature = minutes.toString();
+    // Tạo chữ ký sử dụng service hoặc logic bạn đã có
+    const signature = this.signatureService.generateSignature(dataForSignature);
+
+    // Thêm chữ ký vào header
+    return request.clone({
+      setHeaders: {
+        'Content-Type': 'application/json',
+        'X-Signature': signature,
+      },
+    });
   }
 }
