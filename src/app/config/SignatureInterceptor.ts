11d67e22bb91726e5032dcdf6332f1c84276dc45
trongthuong96 <dotrongthuong8@gmail.com>
2023-12-31 19:15:51 +0700

    change SignatureInterceptor retry 2

--- src/app/config/SignatureInterceptor.ts
+++ src/app/config/SignatureInterceptor.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpEvent,
-  HttpInterceptor,
-  HttpHandler,
-  HttpRequest,
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
+import { Observable, throwError, of } from 'rxjs';
+import { catchError, retryWhen, delay, mergeMap } from 'rxjs/operators';
 import { SignatureService } from '../services/signature.service';
 
 @Injectable()
 export class SignatureInterceptor implements HttpInterceptor {
   
   private lastSignatureTime: number = 0; // Lưu thời điểm cuối cùng chữ ký được tạo
+  private maxRetryAttempts = 2; // Số lần thử lại tối đa
+  private retryDelay = 1000; // Thời gian chờ giữa các lần thử lại (ms)
 
   constructor(private signatureService: SignatureService) {}
 
@@ -38,7 +36,24 @@ export class SignatureInterceptor implements HttpInterceptor {
       },
     });
 
-    // Chuyển tiếp yêu cầu đã được ký đến next handler
-    return next.handle(signedRequest);
+    // Intercept và thử lại lần 1
+    return next.handle(signedRequest)
+      .pipe(
+        retryWhen(errors => errors.pipe(
+          mergeMap((error, i) => {
+            if (i < this.maxRetryAttempts - 1 && error instanceof HttpResponse && error.status === 401) {
+              // Thử lại nếu là lỗi 401 (Unauthorized)
+              return of(error);
+            }
+            return throwError(error);
+          }),
+          delay(this.retryDelay)
+        )),
+        catchError((error) => {
+          // Xử lý lỗi sau khi đã thử lại
+          // Có thể thêm logic xử lý lỗi ở đây
+          return throwError(error);
+        })
+      );
   }
 }
