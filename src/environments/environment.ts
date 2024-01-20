2117f5ac869bc9c8d1f4deea11e1fddf08dd0116
trongthuong96 <dotrongthuong8@Gmail.com>
2024-01-20 18:47:00 +0700

    change api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@
 export const environment = {
     production: true,
     //apiUrl: 'https://localhost:7131/api', // Đường dẫn đến máy chủ proxy trên production
-    apiUrl: 'https://api.truyenmoi.click/api', // Đường dẫn đến máy chủ proxy trên production
-    //apiUrl: 'https://truyenhay.bsite.net/api',
+    //apiUrl: 'https://api.truyenmoi.click/api', // Đường dẫn đến máy chủ proxy trên production
+    apiUrl: 'https://truyenhay.bsite.net/api',
     XAPPSOURCEVALUE: "CfDJ8MwMmTfyyCZNlUEZuFwp4KWu4RiUPEMJTn-wnDzIC3Dq0mouXvZgLtfdwaGVaz891YZ4qBqFsFJQ5laOyFJofH8mwo2G6AjAOYo6crVoAyZZd-D4AfjzmX5oKMhySPRyY4YijkELuvcqpR1yhQubIV0",
     XAPPSOURCENAME: "X-APP-SOURCE",
     UserCookie: "uToken",
